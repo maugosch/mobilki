33d75277545091284afbbbb4a67924b495c9f618
Bartek Korytkowski <asaser11@gmail.com>
2017-05-25 22:33:41 +0200

    pierwsze zmiany

--- app/src/main/java/com/example/sony/financialapp/MainActivity.java
+++ app/src/main/java/com/example/sony/financialapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sony.financialapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void firstFinancialRatio(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void firstBankruptcyModel(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
